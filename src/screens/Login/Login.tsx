533510fb03baac0c110e1ad1976985260117ee0a
iamgaddiel <ighotagaddiel98@gmail.com>
2023-04-05 09:42:21 -0400

    fix onboarding next btn

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,5 +1,5 @@
 import { IonButton, IonButtons, IonContent, IonIcon, IonImg, IonInput, IonLabel, IonPage, IonRouterLink } from '@ionic/react'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 
 // styles
@@ -11,10 +11,28 @@ import Logo from "../../assets/images/login_logo.svg"
 import SpaceBetween from '../../components/style/SpaceBetween'
 import { eyeOffOutline, eyeOutline, logoFacebook, logoGoogle } from 'ionicons/icons'
 import OrSeperator from '../../components/OrSeperator/OrSeperator'
+import { effect, useSignal } from '@preact/signals-react'
 
 
 
 const Login = () => {
+  console.log("rendered")
+  // states
+  const [showPassword, setShowPassword] = useState(false)
+
+
+  // refs
+
+
+  // effects
+
+
+  // functions
+
+
+
+
+
   return (
     <IonPage>
       <IonContent className="ion-padding">
@@ -49,8 +67,20 @@ const Login = () => {
             {/* Password */}
             <div className='form_inputs ion-margin-vertical'>
               <IonLabel>Password</IonLabel>
-              <IonInput type='password' placeholder='Enter your Email' className='mt-2 p-2' />
-              <IonIcon icon={eyeOffOutline} className='input_icon' size='large' slot='end' />
+              <IonInput
+                type={showPassword ? "text" : "password"}
+                placeholder='Enter your Email'
+                className='mt-2 p-2'
+              />
+
+              {/* Toggle Password Visibility */}
+              <IonIcon
+                icon={showPassword ? eyeOutline : eyeOffOutline}
+                className='input_icon'
+                size='large'
+                slot='end'
+                onClick={() => setShowPassword(!showPassword)}
+              />
 
               {/* Forget Password  */}
               <div className="ion-text-end login_nav_btns">
@@ -83,12 +113,12 @@ const Login = () => {
 
 
           <SpaceBetween className='mt-3 mx-auto ion-padding-horizontal'>
-            <IonButton shape='round' className='nm_btn'>
+            <IonButton shape='round' className='nm_btn' color={'light'} >
               <IonIcon icon={logoGoogle} slot='start' size='large' />
               Google
             </IonButton>
-            <IonButton shape='round' className='nm_btn'>
-              <IonIcon icon={logoFacebook} slot='start' size='large'/>
+            <IonButton shape='round' className='nm_btn' color={"primary"}>
+              <IonIcon icon={logoFacebook} slot='start' size='large' />
               Facebook
             </IonButton>
           </SpaceBetween>
--- src/screens/OnBoardidng/OnBoardidng.tsx
+++ src/screens/OnBoardidng/OnBoardidng.tsx
@@ -1,8 +1,8 @@
 import { IonPage, IonContent, IonImg, IonButton } from '@ionic/react'
-import React from 'react'
+import React, { useState } from 'react'
 import SpaceBetween from '../../components/style/SpaceBetween'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
 
 // styles
@@ -16,6 +16,7 @@ import View from "../../assets/images/view.png"
 import P1 from "../../assets/images/1.svg"
 import P2 from "../../assets/images/2.svg"
 import P3 from "../../assets/images/3.svg"
+import { useHistory } from 'react-router';
 
 
 
@@ -27,23 +28,67 @@ const OnBoardidng = () => {
             heading: "Tavel with no worry",
             subText: "You can now experince the next level of travel experince for hote bookings",
             pagination: P1,
-            btnText: "Next"
+            btnText: "Next",
+            slideIndex: 0
         },
         {
             image: People,
             heading: "Find Hundereds of hotels",
             subText: "Discover hundreds of hotels that spread across the world for you",
             pagination: P2,
-            btnText: "Next"
+            btnText: "Next",
+            slideIndex: 1
         },
         {
             image: View,
             heading: "Let's discovery the world",
             subText: "Book hotel for the best travel experience. Enjoy your trip!",
             pagination: P3,
-            btnText: "Get Started"
+            btnText: "Get Started",
+            slideIndex: 2
         },
     ]
+
+    // 3party hooks
+    const history = useHistory()
+
+
+    // states
+    const [swiper, setSwiper] = useState<any>(null)
+    const [nextBtnText, setNextBtnText] = useState("Next")
+    const [paginationImage, setPaginationImage] = useState(P1)
+
+
+
+    // functions
+    function handleSlide() {
+        if (!swiper.isEnd) {
+            swiper.slideNext()
+            return
+        }
+        
+        history.push("/login")
+    }
+
+    function handleSlideChange(){
+        switch (swiper.activeIndex) {
+            case 0:
+                setPaginationImage(P1)
+                break
+
+            case 1:
+                setPaginationImage(P2)
+                break
+
+            case 2:
+                setPaginationImage(P3)
+                break
+        }
+
+        swiper.isEnd && setNextBtnText("Get Started")
+
+    }
+
 
 
     return (
@@ -53,13 +98,14 @@ const OnBoardidng = () => {
                 <Swiper
                     spaceBetween={50}
                     slidesPerView={1}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => handleSlideChange()}
                     pagination={true}
+                    allowSlidePrev={false}
+                    onSwiper={(swp) => setSwiper(swp)}
                 >
                     {
                         slides.map((slide) => (
-                            <SwiperSlide>
+                            <SwiperSlide key={slide.slideIndex}>
                                 <section className="onboarding_images">
                                     <div className="hero_image">
                                         <IonImg src={slide.image} />
@@ -71,23 +117,24 @@ const OnBoardidng = () => {
                                     </div>
                                 </section>
 
-                                <SpaceBetween className='px-4'>
-                                    <IonImg src={slide.pagination} />
-
-                                    <IonButton
-                                        shape="round"
-                                        className='fill brown_fill nm_btn'
-                                        size='large'
-                                        routerLink='/login'
-
-                                    > {slide.btnText}</IonButton>
-                                </SpaceBetween>
 
                             </SwiperSlide>
                         ))
                     }
-
                 </Swiper>
+
+                <SpaceBetween className='px-4'>
+                    <IonImg src={paginationImage} />
+
+                    <IonButton
+                        shape="round"
+                        className='fill brown_fill nm_btn'
+                        size='large'
+                        onClick={() => handleSlide()}
+                    >
+                        {nextBtnText}
+                    </IonButton>
+                </SpaceBetween>
             </IonContent>
         </IonPage>
     )
