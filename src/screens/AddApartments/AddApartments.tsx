76bb35299fa8e18e738decb893dbd55c7be92f4d
iamgaddiel <ighotagaddiel98@gmail.com>
2023-06-29 15:05:33 -0400

    t betransfered

--- src/screens/AddApartments/AddApartments.tsx
+++ src/screens/AddApartments/AddApartments.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonContent, IonInput, IonLabel, IonPage, IonRadio, IonRadioGroup, IonTextarea } from '@ionic/react'
+import { IonButton, IonCheckbox, IonContent, IonInput, IonItemDivider, IonLabel, IonPage, IonRadio, IonRadioGroup, IonSelect, IonSelectOption, IonTextarea } from '@ionic/react'
 import React from 'react'
 import BackHeader from '../../components/BackHeader/BackHeader'
 import SpaceBetween from '../../components/style/SpaceBetween'
@@ -9,117 +9,160 @@ const AddApartments = () => {
             <BackHeader title='Add Apartment' backLink='/appartments' />
             <IonContent className='ion-padding' fullscreen>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>Tile</IonLabel>
+                    <small>Tile</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" placeholder='Apartment title' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal' >
-                    <IonLabel>Description</IonLabel>
+                    <small>Description</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonTextarea placeholder='Enter apartment description' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>Address</IonLabel>
+                    <small>Address</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" placeholder='123 John Street' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>City</IonLabel>
+                    <small>City</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" placeholder='Free Town' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>State</IonLabel>
+                    <small>State</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" placeholder='Lagos' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>Country</IonLabel>
+                    <small>Country</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" placeholder='Nigeria' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>Enter number of Guets</IonLabel>
+                    <small>Enter number of Guets</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" inputMode='numeric' placeholder='Guests' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>Enter number of Beds</IonLabel>
+                    <small>Enter number of Beds</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
                         <IonInput type="text" inputMode='numeric' placeholder='Beds' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <IonLabel>Enter number of Bathrooms</IonLabel>
+                    <small>Enter number of Bathrooms</small>
                     <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
-                        <IonInput type="text" inputMode='numeric' placeholder='Bathrooms' />
+                        <IonInput type="text" inputMode='numeric' placeholder='4 Bathrooms' />
                     </div>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
+                    <small>Appartment Type</small>
+                    <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
+                        <IonSelect mode='ios' placeholder='Single Room' color={'warning'}>
+                            <IonSelectOption>Loft</IonSelectOption>
+                            <IonSelectOption>Micro Appartment</IonSelectOption>
+                            <IonSelectOption>Duplex</IonSelectOption>
+                            <IonSelectOption>Triplex</IonSelectOption>
+                            <IonSelectOption>Co-up</IonSelectOption>
+                            <IonSelectOption>Garden Apartment</IonSelectOption>
+                            <IonSelectOption>Hight-Rise</IonSelectOption>
+                            <IonSelectOption>Mid-Rise</IonSelectOption>
+                            <IonSelectOption>Low-Rise</IonSelectOption>
+                            <IonSelectOption>Railroad</IonSelectOption>
+                            <IonSelectOption>Single Family</IonSelectOption>
+                            <IonSelectOption>Condo</IonSelectOption>
+                        </IonSelect>
+                    </div>
+                </div>
+
+                {/* 
+                -----------------------------------------------------------------
+                ----------------------------- Available Items -------------------
+                -----------------------------------------------------------------
+                 */}
+                <div className="mt-1 ion-padding">
+                    <small className="text-muted">Select Items Available</small>
+                </div>
+                <div className='ion-margin-vertical ion-padding-horizontal'>
                     <SpaceBetween>
-                        <IonLabel>Wifi Available</IonLabel>
-                        <IonRadio />
+                        <small>Wifi</small>
+                        <IonCheckbox mode='ios' color='warning' />
                     </SpaceBetween>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
                     <SpaceBetween>
-                        <IonLabel>TV Cable Available</IonLabel>
-                        <IonRadio />
+                        <small>TV Cable</small>
+                        <IonCheckbox mode='ios' color='warning' />
                     </SpaceBetween>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
                     <SpaceBetween>
-                        <IonLabel>Security Available</IonLabel>
-                        <IonRadio />
+                        <small>Security</small>
+                        <IonCheckbox mode='ios' color='warning' />
                     </SpaceBetween>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
                     <SpaceBetween>
-                        <IonLabel>Gym Available</IonLabel>
-                        <IonRadio />
+                        <small>Gym</small>
+                        <IonCheckbox mode='ios' color='warning' />
                     </SpaceBetween>
                 </div>
                 <div className='ion-margin-vertical ion-padding-horizontal'>
                     <SpaceBetween>
-                        <IonLabel>Laundary Available</IonLabel>
-                        <IonRadio />
-                    </SpaceBetween>
-                </div>
-                <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <SpaceBetween>
-                        <IonLabel>Smoking Allowed</IonLabel>
-                        <IonRadio />
-                    </SpaceBetween>
-                </div>
-                <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <SpaceBetween>
-                        <IonLabel>Pets Allowed</IonLabel>
-                        <IonRadio />
-                    </SpaceBetween>
-                </div>
-                <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <SpaceBetween>
-                        <IonLabel>Children Allowed</IonLabel>
-                        <IonRadio />
-                    </SpaceBetween>
-                </div>
-                <div className='ion-margin-vertical ion-padding-horizontal'>
-                    <SpaceBetween>
-                        <IonLabel>Gym Available</IonLabel>
-                        <IonRadio />
+                        <small>Laundary</small>
+                        <IonCheckbox mode='ios' color='warning' />
                     </SpaceBetween>
                 </div>
 
-                <small className="block mt-1 text-muted ion-padding-horizontal">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, cumque?
-                </small>
+                {/* 
+                -----------------------------------------------------------------
+                ----------------------------- Available Items -------------------
+                -----------------------------------------------------------------
+                 */}
+                <div className="mt-2 ion-padding">
+                    <small className="text-muted">Select Applied Rules</small>
+                </div>
+                <div className='mt-2 ion-padding-horizontal'>
+                    <SpaceBetween>
+                        <small>Smoking Allowed</small>
+                        <IonCheckbox mode='ios' color='warning' />
+                    </SpaceBetween>
+                </div>
+                <div className='ion-margin-vertical ion-padding-horizontal'>
+                    <SpaceBetween>
+                        <small>Pets Allowed</small>
+                        <IonCheckbox mode='ios' color='warning' />
+                    </SpaceBetween>
+                </div>
+                <div className='ion-margin-vertical ion-padding-horizontal'>
+                    <SpaceBetween>
+                        <small>Children Allowed</small>
+                        <IonCheckbox mode='ios' color='warning' />
+                    </SpaceBetween>
+                </div>
+
+                <div className='mt-4 ion-padding-horizontal'>
+                    <small>State Additional Rules</small>
+                    <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
+                        <IonTextarea placeholder='Lights out by 10:00pm' />
+                    </div>
+                </div>
+
+                <div className='ion-padding-horizontal'>
+                    <small>Number of Children allowed </small>
+                    <div className="rounded-5 mt-2" style={{ backgroundColor: "var(--white-4)" }}>
+                        <IonInput type="text" inputMode='numeric' placeholder='6 Children' />
+                    </div>
+                    <small className="text-muted">Enter max number of children allowed</small>
+                </div>
+
 
                 <IonButton
                     className="yellow_fill mt-5"
